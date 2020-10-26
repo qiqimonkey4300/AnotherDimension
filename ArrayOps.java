1cb79d7c764927e174f537222f09e247b484004b
qiqimonkey4300 <yueqi04@gmail.com>
2020-10-26 08:58:22 -0400

    largestInRows() added, compiles successfully

--- ArrayOps.java
+++ ArrayOps.java
@@ -22,4 +22,11 @@ public class ArrayOps {
     }
     return a;
   }
+  public static int[] largestInRows(int[][] matrix) {
+    int[] a = new int[matrix.length];
+    for (int i = 0; i < matrix.length; i++) {
+      a[i] = largest(matrix[i]);
+    }
+    return a;
+  }
 }
