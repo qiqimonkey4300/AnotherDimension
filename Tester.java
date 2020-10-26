e42e57ad9577c00f19e8870401e3de1aebfb2177
qiqimonkey4300 <yueqi04@gmail.com>
2020-10-26 14:42:08 -0400

    4a - e work as expected with examples

--- Tester.java
+++ Tester.java
@@ -1,5 +1,11 @@
+import java.util.Arrays;
 public class Tester {
-  public static void main() {
-
+  public static void main(String[] args) {
+    int[][] A = {{1,0,12,-1}, {7,-2,2,1}, {-5,-2,2,-9}};
+    int[] B = {1, 3, 5};
+    System.out.println(Arrays.toString(ArrayOps.sumRows(A)));
+    System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
+    System.out.println(ArrayOps.sum(B));
+    System.out.println(ArrayOps.sum(A));
   }
 }
