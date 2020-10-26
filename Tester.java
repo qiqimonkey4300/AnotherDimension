515dfa777dbef9abc4c846742c1efb69464346b8
qiqimonkey4300 <yueqi04@gmail.com>
2020-10-26 16:50:38 -0400

    sumCols(int[][] matrix) works with example as expected

--- Tester.java
+++ Tester.java
@@ -7,5 +7,6 @@ public class Tester {
     System.out.println(Arrays.toString(ArrayOps.largestInRows(A)));
     System.out.println(ArrayOps.sum(B));
     System.out.println(ArrayOps.sum(A));
+    System.out.println(Arrays.toString(ArrayOps.sumCols(A)));
   }
 }
