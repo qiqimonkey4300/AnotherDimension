f17b449258e5c909f036cdc20511ddc447a856c8
qiqimonkey4300 <yueqi04@gmail.com>
2020-10-26 16:49:52 -0400

    sumCols(int[][] matrix) added, compiles successfully

--- ArrayOps.java
+++ ArrayOps.java
@@ -33,5 +33,20 @@ public class ArrayOps {
     int x = sum(sumRows(arr));
     return x;
   }
-
+  public static int[] arrayCols(int[][] arr, int cols) {
+    //arranges all values at a certain column into an array
+    int[] a = new int[arr.length];
+    for (int i = 0; i < arr.length; i++) {
+      a[i] = arr[i][cols];
+    }
+    return a;
+  }
+  public static int[] sumCols(int[][] matrix) {
+    int[] a = new int[matrix[0].length];
+    for (int i = 0; i < matrix[0].length; i++) {
+      a[i] = sum(arrayCols(matrix, i));
+    }
+    return a;
+  }
+  
 }
